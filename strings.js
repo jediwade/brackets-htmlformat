7f69fbc1ff9111f82a211c039dd6ee30bf2bb7d2
Wade <jediwade@users.noreply.github.com>
2015-06-12 14:53:28 -0400

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,62 @@
+/*
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50, eqeq: true, white: true */
+/*global define */
+define({
+    "TITLE_MENU"						: "HTML Format",
+	"TITLE_PREFERENCES"					: "HTML Format Preferences",
+    "TITLE_CHARACTER"					: "Charater to be used for the keyboard shortcut.",
+    "TITLE_CTRL"						: "Use the Control key for the keyboard shortcut modifier(s).",
+    "TITLE_CMD"							: "Use the Command (⌘) key for the keyboard shortcut modifier(s).",
+    "TITLE_SHIFT"						: "Use the Shift key for the keyboard shortcut modifier(s).",
+    "TITLE_ALT"							: "Use the Alt key for the keyboard shortcut modifier(s).",
+	"SHORTCUTS_WARNING_1"				: "WARNING: Keyboard shortcuts chosen can override those built into Brackets.",
+	"SHORTCUTS_WARNING_2"				: "Brackets should be reloaded after saving any keyboard shortcut change.",
+    "MODIFIER_CONTROL_CMD"				: "Control",
+	"MODIFIER_CONTROL"					: "Control",
+	"MODIFIER_CMD"						: "Command",
+    "MODIFIER_SHIFT"					: "Shift",
+    "MODIFIER_ALT"						: "Alt",
+    "BOLD_STRONG"						: "Use <strong> tag in place of <b>",
+	"ITALIC_EM"							: "Use <em> tag in place of <i>",
+    "LABEL_BOLD_TAG_SHORTCUT"			: "Bold - tag",
+	"LABEL_BOLD_STYLE_SHORTCUT"			: "Bold - style",
+    "LABEL_ITALIC_TAG_SHORTCUT"			: "Italic - tag",
+	"LABEL_ITALIC_STYLE_SHORTCUT"		: "Italic - style",
+    "LABEL_UNDERLINE_TAG_SHORTCUT"		: "Underline - tag",
+	"LABEL_UNDERLINE_STYLE_SHORTCUT"	: "Underline - style",
+    "LABEL_STRIKE_TAG_SHORTCUT"			: "Strike - tag",
+	"LABEL_STRIKE_STYLE_SHORTCUT"		: "Strike - style",
+    "LABEL_TELETYPE_TAG_SHORTCUT"		: "Teletype - tag",
+	"LABEL_TELETYPE_STYLE_SHORTCUT"		: "Teletype - style",
+	"LABEL_CODE_TAG_SHORTCUT"			: "Code",
+	"LABEL_VARIABLE_TAG_SHORTCUT"		: "Variable",
+	"LABEL_SAMPLE_TAG_SHORTCUT"			: "Sample",
+	"LABEL_KEYBOARD_TAG_SHORTCUT"		: "Keyboard",
+	"LABEL_CITATION_TAG_SHORTCUT"		: "Citation",
+	"LABEL_DEFINITION_TAG_SHORTCUT"		: "Definition",
+	"LABEL_DELETED_TAG_SHORTCUT"		: "Deleted",
+	"LABEL_INSERTED_TAG_SHORTCUT"		: "Inserted",
+    "LABEL_EMPTY_TAG_SHORTCUT"			: "Insert empty tag",
+	"LABEL_PREFERENCES_SHORTCUT"		: "HTML Format Preferences",
+    "BTN_CANCEL"						: "Cancel",
+	"BTN_SAVE"							: "Save"
+});
